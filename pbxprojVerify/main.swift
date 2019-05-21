8086184ee048da09af67687b2aaa468d4ffa01ab
Vladislav Usachev <Usachov.V.An@sberbank.ru>
2019-05-21 21:45:58 +0300

    Initial Commit

---
+++ pbxprojVerify/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  pbxprojVerify
+//
+//  Created by Владислав Усачёв on 21/05/2019.
+//  Copyright © 2019 Владислав Усачёв. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
